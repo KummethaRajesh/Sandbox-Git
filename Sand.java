6533e857eba04024d429815da0130a6e567cc260
Rajeshkumar Reddy <49584808+KummethaRajesh@users.noreply.github.com>
2019-04-13 16:32:47 +0530

    Random class added

--- Sand.java
+++ Sand.java
@@ -2,4 +2,11 @@ class Sand{
   static public void main(String[] args){
     System.out.println("I am Github Sandbox.");
   }
+  
+  class Random{
+    String s="Rajesh";
+    void display(){
+      System.out.println(s);
+    }
+  }
 }
